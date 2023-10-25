54c3f8e5dc752c15c6cd421c5c9845628e4ab732
ahmadbagwi <ahmadbagwi.id@gmail.com>
2023-10-25 15:31:58 +0700

    +dir, base_path

--- doSpacesDownloadBoto3.py
+++ doSpacesDownloadBoto3.py
@@ -13,12 +13,12 @@ class Download:
         self.d = d
     def getByCat(self):
         url = f"{config('URL')}/{self.b}/{self.c}/{self.d}"
-        response = requests.get(url)
-        list = response.json()
+        users = requests.get(url)
+        lists = users.json()
         slug_name = Slugify(to_lower = True)
         slug_name.separator = '_'
-        for j in list:
-            for key, val in j.items():
+        for list in lists:
+            for key, val in list.items():
                 if (key == 'name'):
                     # print(slug_name(val))
                     self.downloadFiles(slug_path = f"2023/{slug_name(val)}")
@@ -32,8 +32,20 @@ class Download:
             dir = 'pkki'
         elif (self.a== 'prp'):
             dir = 'prp'
+        elif (self.a== 'kopk'):
+            dir = 'kopk'
+        elif (self.a== 'dkpm'):
+            dir = 'dkpm'
+        elif (self.a== 'es'):
+            dir = 'es'
+        elif (self.a== 'fbk'):
+            dir = 'fbk'
+        elif (self.a== 'fbk_ib'):
+            dir = 'fbk_ib'
+        else:
+            dir = 'file'
         
-        new_dir_path = f"/home/ahmad-wsl22/web/python/dani/{dir}/{slug_path}"
+        new_dir_path = f"{config('BASE_PATH')}/{dir}/{slug_path}"
         if os.path.isdir(new_dir_path) == False:
             os.mkdir(new_dir_path)
 
@@ -41,17 +53,17 @@ class Download:
         client = session.client('s3',
                     config=botocore.config.Config(s3={'addressing_style': 'virtual'}),
                     region_name=config('REGION_NAME'),
-                    endpoint_url='https://sgp1.digitaloceanspaces.com',
+                    endpoint_url=config('ENDPOINT_URL'),
                     aws_access_key_id=config('PUBLIC_KEY'),
                     aws_secret_access_key=config('SECRET_KEY')
                 )
-        response = client.list_objects(Bucket=config('SPACE_NAME'), Prefix = slug_path, MaxKeys = 1000)
-        for obj in response['Contents']:
-            if (os.path.isfile( f"/home/ahmad-wsl22/web/python/dani/{dir}/{obj['Key']}")):
+        files = client.list_objects(Bucket=config('SPACE_NAME'), Prefix = slug_path, MaxKeys = 1000)
+        for obj in files['Contents']:
+            if (os.path.isfile( f"{config('BASE_PATH')}/{dir}/{obj['Key']}")):
                 print(f"file sudah ada, lewati {obj['Key']}")
             else:
                 client.download_file(config('SPACE_NAME'),
                     obj['Key'],
-                    f"/home/ahmad-wsl22/web/python/dani/{dir}/{obj['Key']}"
+                    f"{config('BASE_PATH')}/{dir}/{obj['Key']}"
                 )
                 print(f"berhasil download {obj['Key']}")
